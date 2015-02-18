f53f330b99a45781f4ade30f1b4ab22c6b8aa887
Leon Graumans <leongraumans@outlook.com>
2015-02-18 10:01:27 +0100

    Modified .gitignore + added country.py

---
+++ country.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#Kamil Zukowski en Leon Graumans
+#[teamoptimel]
+
+import sys
+
+class Country:
+	
+	def __init__(self, country):
+		self.country = country
+	
+	def __str__(self):
+		greet =  'Hello from' + ' ' + self.country +'.'
+		return greet
+
+
+def main(argv):
+	land = Country(argv[1])
+	print(land.__str__())
+
+
+if __name__ == '__main__':
+	main(sys.argv)
