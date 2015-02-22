d31b769ffb1720391ff41c2a8fc0aa670c1678d1
Leon Graumans <leongraumans@outlook.com>
2015-02-22 13:35:26 +0100

    Updated flag_ui.py and getdata.py

--- flag_ui.py
+++ flag_ui.py
@@ -3,51 +3,59 @@
 
 import sys
 from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
 
 class FlagFrame(QtGui.QWidget):
 	
 	def __init__(self):
 		super(FlagFrame, self).__init__()
-		
+
 		self.initUI()
 		
 	def initUI(self):
 		
-		self.lbl = QtGui.QLabel("Country", self)
+		#self.countries = sorted(self.countries.keys())
+		
+		self.lbl = QtGui.QLabel("Welcome, choose a country.", self)
 		data = self.getCountry()
-		self.fromComboBox = QComboBox()
-		self.fromComboBox.addItems(countries)
-		
-		self.fromComboBox.activated[str].connect(self.onActivated)
-		
+		self.ComboBox = QtGui.QComboBox(self)
+		self.ComboBox.addItems(self.countries)
+		#self.ComboBox.activated[str].connect(self.onActivated)
+		self.ComboBox.move(0,50)
+	
 		self.setGeometry(300, 300, 300, 200)
 		self.setWindowTitle("Flagpicker")
 		self.show()
 		
-	def onActivated(self, text):
+	#def onActivated(self, text):
 		
-		self.lbl.setText(text)
-		self.lbl.adjustSize()
+		#self.lbl.setText(text)
+		#self.lbl.adjustSize()
 		
 		
 	def getCountry(self):
-		
+
 		self.countries = []
 		with open('countries_list.txt', 'r') as infile:
-			for line in infile:
-				for countries in line.split():
-					return(countries)
-					
-
+			for lines in infile:
+				lines = lines.rstrip('\n')
+				self.countries.append((lines))
 	
+	#def getCountry(self):
 		
-		
+		#self.countries = []
+		#with open('countries_list.txt', 'r') as infile:
+			#for line in infile:
+				#for countries in line.split():
+					#return(countries)
+	
 def main():
 	
 	app = QtGui.QApplication(sys.argv)
 	form = FlagFrame()
-	form.show()
-	app.exec_()
+	sys.exit(app.exec_())
 	
 	
 if __name__ == "__main__":
--- getdata.py
+++ getdata.py
@@ -1,9 +1,10 @@
 def getCountry():
 
-    with open('countries_list.txt', 'r') as infile:
-        for line in infile:
-            for country in line.split():
-                print(country)
-
+	with open('countries_list.txt', 'r') as infile:
+		for line in infile:
+			line = line.rstrip('\n') #haalt newlines onder elke line weg
+			print (line) #heb 'm zo gelaten, als je de line nog gaat strippen haalt ie landen United States ook uit elkaar
+           # for country in line.strip():
+                #print(country)
 
 getCountry()
